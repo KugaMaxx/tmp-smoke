198d96edc24c9b0f3bdb7d947fdb1edd7688b819
KugaMaxx <KugaMaxx@outlook.com>
2025-07-07 08:43:09 +0800

    Update

---
+++ Python/examples/train-a-model/train-clip.sh
@@ -0,0 +1,10 @@
+python train-clip.py \
+    --output_dir ./clip-roberta-finetuned \
+    --dataset_name "/home/dszh/workspace/tmp-smoke/Python/data/cube-texture-clip" \
+    --do_train  --do_eval \
+    --per_device_train_batch_size="64" \
+    --per_device_eval_batch_size="64" \
+    --learning_rate="5e-5" --warmup_steps="0" --weight_decay 0.1 \
+    --overwrite_output_dir \
+    --remove_unused_columns=False \
+    --num_train_epochs 3
